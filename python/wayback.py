b6bc510198f4674043ce02edcdc2e0c0d2d8d182
Keith Abraham <finkybot@outlook.com>
2019-02-11 21:06:51 +0000

    removing python files

--- python/wayback.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-import json
-form urllib.request import urlopen
-
-print("Lets's find an old website.")
-site = input("Type a website URL: ")
-era = "http://archive.org/wayback/available?url=%s&timestamp=%s" % (site, era)
